e7797e5a462c07f509c6b88468cf2eaab4db414c
Björn Kahlert <mail@bkahlert.com>
2015-06-09 07:34:06 +0200

    HTML version added

--- export/js/main.js
+++ export/js/main.js
@@ -10,6 +10,20 @@ $('[href]').each(function() {
   }
 });
 
+var fadeStart=100;
+var fadeUntil=300;
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    $('.navbar[role=navigation]').css('opacity',opacity);
+});
+
 });
 
 })(jQuery);
